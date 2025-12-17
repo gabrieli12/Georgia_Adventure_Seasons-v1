53804b0e3a77f3fd4ac0c5986213a2be58d4c1c5
gabrieli <gabrielmolodini2@gmail.com>
2025-12-17 23:38:32 +0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+      <div className='w-full h-screen flex justify-center items-center bg-black'>
+        <h1 className='text-5xl font-medium tracking-wider text-green-600'>Georgia Adventure Seasons</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
