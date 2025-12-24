f19f0a009fabf7cc8425d6d655fc08a682644e5a
goga574 <discordforgoa@gmail.com>
2025-12-24 17:36:29 +0400

    Goga's push

--- src/HOME/GridCarservices/Gridimgs.jsx
+++ src/HOME/GridCarservices/Gridimgs.jsx
@@ -3,15 +3,15 @@ import React from 'react'
 function Gridimgs() {
     return (
         <>
-            <div className="w-full grid gap-5 p-5  grid-cols-[repeat(auto-fit,minmax(320px,1fr))]  ">
-                <img src="cuadro.png" className="w-full h-full object-cover row-span-2" />
-                <img src="coolview.png" className="w-full h-full object-cover col-span-2" />
-                <img src="kater.png" className="w-full h-full object-cover row-span-2" />
-                <img src="skiing.png" className="w-full h-full object-cover col-start-1" />
-                <img src="parashuti.png" className="w-full h-full object-cover row-span-2 row-start-2 col-start-2" />
-                <img src="jomardoba.png" className="w-full h-full object-cover row-start-2 col-start-3" />
-                <img src="horse.png" className="w-full h-full object-cover" />
-                <img src="lakewalk.png" className="w-full h-full object-cover" />
+            <div className="w-full grid gap-5 p-50  grid-cols-[repeat(auto-fit,minmax(320px,1fr))]   ">
+                <img src="cuadro.jpg" className="w-full h-full object-cover row-span-2 rounded-[5px]" />
+                <img src="hiking.png" className="w-full h-full object-cover col-span-2  rounded-[5px]" />
+                <img src="kater.png" className="w-full h-full object-cover row-span-2 rounded-[5px]" />
+                <img src="skiing.jpg" className="w-full h-full object-cover col-start-1 rounded-[5px]" />
+                <img src="parashuti.jpg" className="w-full h-full object-cover row-span-2 row-start-2 col-start-2 rounded-[5px]" />
+                <img src="jomardoba.png" className="w-full  h-full object-cover row-start-2 col-start-3 rounded-[5px]" />
+                <img src="horse.jpg" className="w-full h-full object-cover rounded-[5px]" />
+                <img src="lakewalk.png" className="w-full h-full object-cover rounded-[5px]" />
             </div>
         </>
     )
