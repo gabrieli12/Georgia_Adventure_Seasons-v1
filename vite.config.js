6421492c48f4041992820b4232f994d3e4160f48
gabrieli <gabrielmolodini2@gmail.com>
2026-05-02 19:43:02 +0400

    new image optimization

--- vite.config.js
+++ vite.config.js
@@ -12,16 +12,20 @@ export default defineConfig({
         plugins: [['babel-plugin-react-compiler']],
       },
     }),
-    viteImagemin({
-      gifsicle: { optimizationLevel: 7, interlaced: false },
-      optipng: { optimizationLevel: 7 },
-      mozjpeg: { quality: 20 },
-      pngquant: { quality: [0.8, 0.9], speed: 4 },
-      svgo: {
-        plugins: [{ name: 'removeViewBox' }, { name: 'removeEmptyAttrs', active: false }],
+    ViteImageOptimizer({
+      /* 
+         ეს კონფიგურაცია ავტომატურად გადაიყვანს ყველაფერს WebP-ში 
+         და შეამცირებს ზომას ისე, რომ ხარისხი არ დაიკარგოს 
+      */
+      webp: {
+        quality: 75,
       },
-      // აქ მიუთითებ WebP-ის პარამეტრებს
-      webp: { quality: 75 }
+      jpeg: {
+        quality: 75,
+      },
+      png: {
+        quality: 70,
+      },
     }),
   ],
 })
