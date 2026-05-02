ebb2f1271d3a37b5ef3105b8dd7a382b481b6db5
gabrieli <gabrielmolodini2@gmail.com>
2026-05-02 19:55:07 +0400

    add optimizer library

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import viteImagemin from 'vite-plugin-imagemin';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 // https://vite.dev/config/
 export default defineConfig({
